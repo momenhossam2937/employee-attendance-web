c5d92cdc840e5fd83fee0857aa7152a25407c205
MomenHossam <Momen.hossam@legate.com.sa>
2023-02-05 17:40:32 +0200

    fix-issue

--- src/app/modules/employee-attendance/models/create-employee-attendance/create-employee-attendance-request.model.ts
+++ src/app/modules/employee-attendance/models/create-employee-attendance/create-employee-attendance-request.model.ts
@@ -1,9 +1,8 @@
 export class EmployeeAttendanceRequest
 {
     eVETLGUID:number=0;
-    dEVDT:Number = 0;
     dEVUID:number=0;
     eMPID:number=0;
-    DEVDT2:Date = new Date();
+    DEVDT:Date = new Date();
 
 }
